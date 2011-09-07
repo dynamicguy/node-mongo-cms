5375e0e1deae8fa40c7ffbffd2b8dfcfc50ae22c
Nurul Ferdous <hello@dynamicguy.com>
2011-09-07 22:07:41 +0600

    bootstrap added

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+<div class="topbar">
+      <div class="fill">
+        <div class="container">
+          <h3><a href="#">Project name</a></h3>
+          <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+</div>
+
 # node-blog
 
   Insanely fast (and small) server-side JavaScript blog application framework
--- app.js
+++ app.js
@@ -1,21 +1,86 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 
-var app = module.exports = express.createServer();
+var mongoose = require('mongoose')
+     , Schema = mongoose.Schema
+     , mongooseAuth = require('mongoose-auth');
+
+var UserSchema = new Schema({})
+     , User;
+
+var conf = require('./conf');
+
+UserSchema.plugin(mongooseAuth, {
+	everymodule: {
+	  everyauth: {
+	      User: function () {
+	        return User;
+	      }
+	  }
+	}
+	, facebook: {
+	  everyauth: {
+	      myHostname: 'http://localhost:3000'
+	    , appId: conf.fb.appId
+	    , appSecret: conf.fb.appSecret
+	    , redirectPath: '/'
+	  }
+	}
+	, twitter: {
+	  everyauth: {
+	      myHostname: 'http://localhost:3000'
+	    , consumerKey: conf.twit.consumerKey
+	    , consumerSecret: conf.twit.consumerSecret
+	    , redirectPath: '/'
+	  }
+	}
+	, password: {
+	    everyauth: {
+	        getLoginPath: '/login'
+	      , postLoginPath: '/login'
+	      , loginView: 'login.jade'
+	      , getRegisterPath: '/register'
+	      , postRegisterPath: '/register'
+	      , registerView: 'register.jade'
+	      , loginSuccessRedirect: '/'
+	      , registerSuccessRedirect: '/'
+		  , loginWith: 'login'
+	    }
+	}
+	, github: {
+	  everyauth: {
+	      myHostname: 'http://localhost:3000'
+	    , appId: conf.github.appId
+	    , appSecret: conf.github.appSecret
+	    , redirectPath: '/'
+	  }
+	}
+	, instagram: {
+	  everyauth: {
+	      myHostname: 'http://localhost:3000'
+	    , appId: conf.instagram.clientId
+	    , appSecret: conf.instagram.clientSecret
+	    , redirectPath: '/'
+	  }
+	}
+});
+
+mongoose.model('User', UserSchema);
+mongoose.connect('mongodb://localhost/example');
+User = mongoose.model('User');
+
+var app = express.createServer();
+mongooseAuth.helpExpress(app);
 
 // Configuration
-
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'esoognom'}));
+  app.use(mongooseAuth.middleware());
 });
 
 app.configure('development', function(){
@@ -27,10 +92,9 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Express'
+    title: 'NodeJS MongoDB CMS'
   });
 });
 
